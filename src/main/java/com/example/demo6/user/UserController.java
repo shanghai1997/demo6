e38089249324e37390fc4d097f0d730f4a126c90
Hong Wang <hongwang@Hongs-MacBook-Pro.local>
2022-04-02 19:42:38 -0230

    data.sql update 3

--- src/main/java/com/example/demo6/user/UserController.java
+++ src/main/java/com/example/demo6/user/UserController.java
@@ -15,7 +15,7 @@ public class UserController {
 
     @GetMapping(value={"/user", "/api/user"})
     @ResponseBody
-    public Object addProduct() {
+    public Object show() {
         return repository.findUserByOrderByScoreDesc();
     }
 
